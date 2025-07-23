bd4881b6c61989c00a95917409d5f7f2ca6ebbca
deaww77 <126137481+deaww77@users.noreply.github.com>
2025-07-23 18:51:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const name = "Chanon";
+  return (
+    <>
+      <h1>Welcome to Forums : {name} </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+        voluptatibus.
+      </p>
+    </>
+  );
+}
+export default App;
